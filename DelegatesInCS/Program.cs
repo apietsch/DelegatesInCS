cb0daf8264f98c863b900fcc6ebcd4717b09dc63
alex <alexander.pietsch@googlemail.com>
2020-07-12 13:06:14 +0200

    init

---
+++ DelegatesInCS/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DelegatesInCS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
